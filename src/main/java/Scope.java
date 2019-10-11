a7acbfe053270b162e282f61f6428055301bac73
Alexey <alexey.rykhalskiy@gmail.com>
2019-10-11 21:43:18 +0400

    -- Scope.java

---
+++ src/main/java/Scope.java
@@ -0,0 +1,13 @@
+public class Scope {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        int k = 5;
+        if (k == 5) {
+            int zz= 777;
+//            int k = 7;
+        }
+//        zz = 888;
+    }
+}
