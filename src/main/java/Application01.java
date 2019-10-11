ce01a14cd85458424234834bb4860b1486b62d35
Alexey <alexey.rykhalskiy@gmail.com>
2019-10-11 16:47:25 +0400

    v2

--- src/main/java/Application01.java
+++ src/main/java/Application01.java
@@ -1,10 +1,18 @@
+
+
 import java.util.Scanner;
 
 public class Application01 {
+
     public static void main(String[] args) {
+
         Scanner s = new Scanner(System.in);
+
         System.out.print("Where are you from? ");
+
         String city = s.nextLine();
+
         System.out.printf("Hello from %s!", city);
     }
+
 }
