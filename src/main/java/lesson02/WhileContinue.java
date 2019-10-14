dfb7f01278f08e5bb34510cf2bf06cd70ec75905
Alexey <alexey.rykhalskiy@gmail.com>
2019-10-14 20:08:57 +0400

    -- WhileContinue

---
+++ src/main/java/lesson02/WhileContinue.java
@@ -0,0 +1,32 @@
+package lesson02;
+
+import java.util.Random;
+
+public class WhileContinue {
+    private static int[] generate() {
+        Random random = new Random();
+        int[] a = new int[30];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = random.nextInt(20) - 10;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int[] generated = generate();
+
+        for (int val: generated) {
+            if (val % 2 == 0) {
+                System.out.printf("%d ",val);
+            }
+        }
+        System.out.println();
+
+        for (int val: generated) {
+            if (val % 2 != 0) continue;
+            System.out.printf("%d ",val);
+        }
+        System.out.println();
+
+    }
+}
