c584798a7b680a1028ab196a823d3c46861b0b73
Alexey <alexey.rykhalskiy@gmail.com>
2019-10-18 13:05:15 +0400

    -- lesson 3 polished -- lesson 4 warmup

---
+++ src/main/java/lesson04/warmup/InvertString.java
@@ -0,0 +1,9 @@
+package lesson04.warmup;
+
+public class InvertString {
+
+  public String perform(String origin) {
+    throw new IllegalArgumentException("must be implemented to pass the tests");
+  }
+
+}
