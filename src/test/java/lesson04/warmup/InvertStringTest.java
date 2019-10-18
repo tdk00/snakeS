239cb6819629d3b3e2827eff730723dd08ef84b6
Alexey <alexey.rykhalskiy@gmail.com>
2019-10-18 14:58:09 +0400

    -- maven goals

---
+++ src/test/java/lesson04/warmup/InvertStringTest.java
@@ -0,0 +1,14 @@
+package lesson04.warmup;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InvertStringTest {
+
+  @Test
+  public void perform() {
+    // just stub to show functionality
+    assertEquals(1,1);
+  }
+}
