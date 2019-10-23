5e7196e0574d3260631348ab2beee6975ef84eb7
Alexey <alexey.rykhalskiy@gmail.com>
2019-10-23 16:15:48 +0400

    -- lesson 6 warmup

---
+++ src/main/java/lesson06/ArrayToColl.java
@@ -0,0 +1,12 @@
+package lesson06;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayToColl {
+  public static void main(String[] args) {
+    List<Integer> from = Arrays.asList(1,2,3);
+  }
+}
