d7cbecef42f1cb75d86f6a87ce898b52e3db53dd
Alexey <alexey.rykhalskiy@gmail.com>
2019-10-28 17:36:21 +0400

    -- lesson 8 warmup

--- src/main/java/lesson08/warmup/Summarizer.java
+++ src/main/java/lesson08/warmup/Summarizer.java
@@ -1,9 +1,41 @@
 package lesson08.warmup;
 
+import java.util.Optional;
+
 public class Summarizer {
 
-  int sum(String origin) {
-    throw new RuntimeException("Not implemented");
+  boolean isNumber(String src) {
+    try {
+      Integer.parseInt(src);
+      return true;
+    } catch (NumberFormatException ex) {
+      return false;
+    }
   }
 
+  int sum_v1(String origin) {
+    if (origin == null) return 0;
+    String[] items = origin.split(" ");
+    int total = 0;
+    for (String item: items) {
+      if (isNumber(item)) {
+        total += Integer.parseInt(item);
+      }
+    }
+    return total;
+  }
+
+  Optional<Integer> convertToInt(String src) {
+    try {
+      int val = Integer.parseInt(src);
+      return Optional.of(val);
+    } catch (NumberFormatException ex) {
+      return Optional.empty();
+    }
+  }
+
+  int sum(String origin) {
+
+    return -1;
+  }
 }
--- src/main/java/lesson08/warmup/SummarizerApp.java
+++ src/main/java/lesson08/warmup/SummarizerApp.java
@@ -1,13 +1,31 @@
 package lesson08.warmup;
 
+import java.util.Optional;
 import java.util.Scanner;
 
 public class SummarizerApp {
-  public static void main(String[] args) {
+  public static void main1(String[] args) {
     Summarizer s = new Summarizer();
     Scanner in = new Scanner(System.in);
     String line = in.nextLine();
     int result = s.sum(line);
     System.out.printf("The sum is:%d\n", result);
+  }
+
+  public static void main(String[] args) {
+    Summarizer s = new Summarizer();
+    Optional<Integer> o1 = s.convertToInt("123");
+    Optional<Integer> o2 = s.convertToInt("mdff435hgmg");
+    String sh = "Hello";
+    String nn = null;
+    Optional<String> opt1 = Optional.ofNullable(sh);
+    Optional<String> opt2 = Optional.ofNullable(nn);
+
+
+    if (o1.isPresent()) {} // true
+    Integer val = o1.get();
+
+    if (o2.isPresent()) {} // false
+    // o2.get(); // will brake
   }
 }
