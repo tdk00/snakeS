3db48269154cef1f1ea5cf34e5fffc55e503304c
Alexey <alexey.rykhalskiy@gmail.com>
2019-10-28 19:00:48 +0400

    -- lesson 8 Summarizer.java

---
+++ src/main/java/lesson08/streams/StreamGenerateApp.java
@@ -0,0 +1,26 @@
+package lesson08.streams;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class StreamGenerateApp {
+  public static void main1(String[] args) {
+    Stream.generate(() -> Math.random()*100)
+        .limit(10)
+        .map(Double::intValue)
+        .forEach(s-> System.out.println(s));
+  }
+
+  public static void main(String[] args) {
+    LinkedList<String> collect = "Hello World".chars()
+        .boxed()
+        .map(i -> String.format("*** %d ***", i))
+        .collect(Collectors.toCollection(LinkedList::new));
+  }
+}
