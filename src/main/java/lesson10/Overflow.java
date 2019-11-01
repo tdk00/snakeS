80ce76d61ef47fc41588ef1f709b41ba36141efd
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-01 21:56:22 +0400

    -- overflow

--- src/main/java/lesson10/Overflow.java
+++ src/main/java/lesson10/Overflow.java
@@ -3,16 +3,24 @@ package lesson10;
 public class Overflow {
 
   static long add(long a, long b) {
-    return a + b;
+    long r = a + b;
+    if ((a < 0 && b < 0 && r > 0) || (a > 0 && b > 0 && r < 0))
+      throw new IllegalArgumentException("Long Overflow");
+    return r;
   }
 
   public static void main(String[] args) {
     long a = 9223372036854775774L;
     long b = 34;
     long c = add(a, b);
-
+    long a1 = Long.MIN_VALUE;
+    long b1 = -2;
+    long c1 = add(a1, b1);
     System.out.println(a);
     System.out.println(b);
     System.out.println(c);
+    System.out.println(a1);
+    System.out.println(b1);
+    System.out.println(c1);
   }
 }
