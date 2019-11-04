781b9abab71e23c607185f8843f8e64814b8c9dd
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-04 20:40:05 +0400

    -- XLinkedList.java

---
+++ src/main/java/lesson11/linkedlist/XLinkedList.java
@@ -0,0 +1,42 @@
+package lesson11.linkedlist;
+
+import java.util.StringJoiner;
+
+public class XLinkedList {
+
+  class XItem {
+    int value;
+    XItem next;
+
+    public XItem(int value) {
+      this.value = value;
+    }
+  }
+
+  private XItem head;
+
+  public void add(int value) {
+    XItem item = new XItem(value);
+    if (head == null) {
+      head = item;
+    } else {
+      XItem current = head;
+
+      while (current.next != null) {
+        current = current.next;
+      }
+      current.next = item;
+    }
+  }
+
+  public boolean contains(int value) {
+    return false;
+  }
+
+  public String toString() {
+    StringJoiner sj = new StringJoiner(",");
+    sj.add();
+    return sj.toString();
+  }
+
+}
