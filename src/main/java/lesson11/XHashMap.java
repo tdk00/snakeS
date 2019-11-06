74745e38d6ae3f1fb921bfe70ecf515ee2cc9bfb
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-06 22:00:48 +0400

    -- XMergeApp

---
+++ src/main/java/lesson11/XHashMap.java
@@ -0,0 +1,24 @@
+package lesson11;
+
+import java.util.LinkedList;
+
+public class XHashMap {
+  private Integer[] data = new Integer[10];
+
+  public int get(int key) {
+    return data[key % 10];
+  }
+
+  public void put(int key, int value) {
+    data[key % 10] = value;
+  }
+
+  public static void main(String[] args) {
+    XHashMap xm = new XHashMap();
+    xm.put(101, 500);
+    xm.put(205, 600);
+    xm.put(305, 601);
+
+  }
+
+}
---
+++ src/main/java/lesson11/linkedlist/LinkedListApp.java
@@ -0,0 +1,24 @@
+package lesson11.linkedlist;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class LinkedListApp {
+  public static void main(String[] args) {
+    LinkedList<Integer> ll = new LinkedList<>();
+    ll.add(1);
+    ll.add(2);
+    ll.add(3);
+    ll.add(4);
+    Iterator<Integer> iterator = ll.iterator();
+    while (iterator.hasNext()) {
+      System.out.println(iterator.next());
+    }
+    Iterator<Integer> diterator = ll.descendingIterator();
+    while (diterator.hasNext()) {
+      System.out.println(diterator.next());
+    }
+
+  }
+}
