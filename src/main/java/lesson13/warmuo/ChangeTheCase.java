942c763b36cf7d7159599845420d4d396814c369
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-08 17:47:12 +0400

    warmup boilerplate code + tests

--- src/main/java/lesson13/warmuo/ChangeTheCase.java
+++ src/main/java/lesson13/warmuo/ChangeTheCase.java
@@ -2,6 +2,10 @@ package lesson13.warmuo;
 
 public class ChangeTheCase {
   String invert(String origin) {
-    throw new IllegalArgumentException("not implemented yet");
+    byte[] bytes = origin.getBytes();
+    for (int i = 0; i < bytes.length; i++) {
+      bytes[i]^=1<<5;
+    }
+    return new String(bytes);
   }
 }
