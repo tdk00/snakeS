507d522733743bce24b18d9e6d9a0e7b21880587
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-13 14:15:18 +0400

    -- code polished a bit

--- src/main/java/lesson13/warmuo/GetRidOfVowels.java
+++ src/main/java/lesson13/warmuo/GetRidOfVowels.java
@@ -4,11 +4,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class GetRidOfVowels {
   String filter(String origin) {
     List<Character> vowels = Arrays.asList('A', 'E', 'I', 'O', 'U');
-//    Arrays.stream(origin.split(""));
+//    Stream<String> stream = Arrays.stream(origin.split(""));
     return origin.chars().mapToObj(c -> (char)c)
         .filter(c -> !vowels.contains(Character.toUpperCase(c)))
         .map(String::valueOf)
