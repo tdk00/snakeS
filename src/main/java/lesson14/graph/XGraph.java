7342d65206cd44f226cfdda27bdfaa6fb7214b9b
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-13 21:40:07 +0400

    -- XGraph.java

---
+++ src/main/java/lesson14/graph/XGraph.java
@@ -0,0 +1,25 @@
+package lesson14.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class XGraph {
+  private int v_count;
+  private List<Integer>[] links;
+
+  public XGraph(int v_count) {
+    this.v_count = v_count;
+    this.links = new ArrayList[v_count];
+    for (int i = 0; i < v_count; i++) {
+      links[i] = new ArrayList<>();
+    }
+  }
+
+  public void add(int from, int to) {
+    links[from].add(to);
+  }
+
+  public void remove(int from, int to) {
+    links[from].remove(Integer.valueOf(to));
+  }
+}
---
+++ src/main/java/lesson14/graph/XGraphApp.java
@@ -0,0 +1,29 @@
+package lesson14.graph;
+
+public class XGraphApp {
+  public static void main(String[] args) {
+    XGraph g = new XGraph(14);
+    g.add(0, 1);
+
+    g.add(1, 2);
+    g.add(1, 3);
+
+    g.add(2, 4);
+    g.add(2, 5);
+
+    g.add(4, 8);
+    g.add(4, 9);
+
+    g.add(5, 10);
+    g.add(5, 11);
+
+    g.add(3, 6);
+    g.add(3, 7);
+
+    g.add(6, 12);
+
+    g.add(7, 13);
+    g.add(7, 14);
+
+  }
+}
