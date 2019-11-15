1d3b224bad157183109c278d915711e2f2bdd806
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-15 16:45:59 +0400

    -- graph polished

---
+++ src/main/java/lesson14/RotateNegatives.java
@@ -0,0 +1,7 @@
+package lesson14;
+
+public class RotateNegatives {
+  public int[] rotate(int[] data) {
+    throw new IllegalArgumentException("not implemented");
+  }
+}
---
+++ src/test/java/lesson14/RotateNegativesTest.java
@@ -0,0 +1,24 @@
+package lesson14;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class RotateNegativesTest {
+
+  private RotateNegatives c;
+
+  @Before
+  public void setUp() throws Exception {
+    this.c = new RotateNegatives();
+  }
+
+  @Test
+  public void rotate() {
+    int[] src =      {7,-6,1,2,-3,3,4,5,-1,6,7,-2,8,9,-3,8};
+    int[] expected = {7,-3,1,2,-6,3,4,5,-3,6,7,-1,8,9,-2,8};
+    int[] rotated  = c.rotate(src);
+    assertArrayEquals(expected, rotated);
+  }
+}
