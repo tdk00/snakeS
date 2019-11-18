60c83a05bce42f1d45baf68cff5220dbd6334e6e
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-18 20:23:42 +0400

    server

--- src/main/java/lesson16/web/CalcServlet.java
+++ src/main/java/lesson16/web/CalcServlet.java
@@ -6,10 +6,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Optional;
 
 public class CalcServlet extends HttpServlet {
 
+  private Optional<Integer> convert(String param) {
+    try {
+      return Optional.of(Integer.parseInt(param));
+    } catch (Exception ignored) {
+      return Optional.empty();
+    }
+  }
+
+  private Optional<Integer> divide(int x, int y) {
+    if (y==0) return Optional.empty();
+    return Optional.of(x/y);
+  }
+
   private String calc(String p1, String p2, String op) {
+    Optional<Integer> p1o = convert(p1);
+    Optional<Integer> p2o = convert(p2);
     return "";
   }
 
