b0b478ed02cf48603cdd7bbd7a2465c1a12dbbe8
Alexey <alexey.rykhalskiy@gmail.com>
2019-11-18 16:53:38 +0400

    -- lesson 16 warmup

--- src/main/java/lesson16/warmup/Task1.java
+++ src/main/java/lesson16/warmup/Task1.java
@@ -2,6 +2,8 @@ package lesson16.warmup;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static lesson16.warmup.Common.list;
 import static lesson16.warmup.Common.combine;
@@ -12,12 +14,41 @@ public class Task1 {
     ArrayList<String> outcome = new ArrayList<>();
     for (int i = 0; i < subjects.size(); i++) {
       for (int j = 0; j < verbs.size(); j++) {
-        for (int k = 0; k < verbs.size(); k++) {
+        for (int k = 0; k < objects.size(); k++) {
           outcome.add(combine(subjects.get(i), verbs.get(j), objects.get(k)));
         }
       }
     }
     return outcome;
+  }
+
+  private List<String> solution2(List<String> subjects, List<String> verbs,List<String> objects) {
+    ArrayList<String> outcome = new ArrayList<>();
+    for (String subj: subjects)
+      for (String verb: verbs)
+        for (String obj: objects)
+          outcome.add(combine(subj, verb, obj));
+    return outcome;
+  }
+
+  private List<String> solution3(List<String> subjects, List<String> verbs,List<String> objects) {
+    ArrayList<String> outcome = new ArrayList<>();
+    subjects.forEach(subj ->
+        verbs.forEach(verb ->
+            objects.forEach(obj ->
+                outcome.add(combine(subj, verb, obj))
+            )));
+    return outcome;
+  }
+
+  private List<String> solution4(List<String> subjects,
+                                 List<String> verbs,
+                                 List<String> objects) {
+    return subjects.stream().flatMap((String subj) ->
+        verbs.stream().flatMap((String verb) ->
+            objects.stream().map((String obj) ->
+                combine(subj, verb, obj)
+    ))).collect(Collectors.toList());
   }
 
   public static void main(String[] args) {
@@ -26,6 +57,6 @@ public class Task1 {
     List<String> objects =  list("the book","the ball","the bed");
     Task1 app = new Task1();
 
-    app.solution1(subjects, verbs, objects).forEach(System.out::println);
+    app.solution4(subjects, verbs, objects).forEach(System.out::println);
   }
 }
